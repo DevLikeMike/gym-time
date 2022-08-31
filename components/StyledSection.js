a3c7f15b32db7301a3bb516ba0339ce289d7b456
DevLikeMike <mhall.project@gmail.com>
2022-08-31 10:59:03 -0400

    added basic styling to sections, changed some class names, added coloring and sizing to sections

--- components/StyledSection.js
+++ components/StyledSection.js
@@ -1,13 +1,47 @@
 import styled from "styled-components";
+// Next Imports
+import Image from "next/image";
 
 const Section = styled.section`
   padding: 1rem 3rem;
   margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1rem;
+
+  .right {
+    order: 2;
+  }
+
+  .section__imgContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    @media (max-width: 500px) {
+      align-items: center;
+    }
+  }
+
+  .section__infoContainer {
+    overflow: hidden;
+    border-radius: 8px;
+  }
+
+  img {
+    object-fit: cover;
+    width: 100%;
+  }
 
   p,
   h2 {
-    max-width: 1200px;
+    max-width: 75rem;
     margin: 0 auto;
+  }
+
+  h2 {
+    font-weight: 500;
   }
 
   @media (max-width: 500px) {
@@ -15,10 +49,19 @@ const Section = styled.section`
   }
 `;
 
-function StyledSection({ children, color }) {
+function StyledSection({ children, color, imgSrc, left }) {
   return (
     <Section className={color ? "section-standard" : "section-white"}>
-      {children}
+      <div
+        className={
+          left ? "section__infoContainer left" : "section__infoContainer right"
+        }
+      >
+        {children}
+      </div>
+      <div className='section__imgContainer'>
+        <Image src={`/imgs/${imgSrc}`} alt={imgSrc} width='250' height='300' />
+      </div>
     </Section>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
 import Hero from "@/components/Hero";
 import Section from "@/components/StyledSection";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <div>
-        <Section color={true}>
-          <h2 className='text-center'>Turn Your New Years into Confetti!!!</h2>
-          <p className='text-center'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
-            incidunt? Eum illum dolor laboriosam nulla sed quia, itaque magni?
-            Laborum nihil quas dicta et eius cumque qui dolorem placeat.
-            Cupiditate debitis nam rem iste autem, pariatur porro aliquam
-            voluptates possimus laboriosam, nisi, dignissimos commodi non!
+        <Section color={true} imgSrc='GymFloor.jpeg' left={true}>
+          <h2>Enroll Today</h2>
+          <p>
+            Bailey's offers many flexible plans to fit your needs. If you're
+            looking fo 24/7 access we got you covered! Need multiple locations
+            or just want to stick to one, choose what is best for you! We have
+            many options for you to choose from!
           </p>
+          <Link href='/plans'>
+            <a className='link'>Enroll Today...</a>
+          </Link>
         </Section>
-        <Section color={false}>
-          <h2 className='text-center'>Summer Is Ending Soon</h2>
-          <p className='text-center'>
+        <Section color={false} imgSrc='GymFloor.jpeg' left={false}>
+          <h2>Summer Is Ending Soon</h2>
+          <p>
             With summer ending school is right around the corner. Don't miss out
             on your chance to join before classes. If you sign up before 9/30/22
             you can save with our Back To School Bundle. Skip the fees and focus
             on your next class with Bailey's Health and fitness.
           </p>
+          <Link href='/plans'>
+            <a className='link'>Browse our plans...</a>
+          </Link>
         </Section>
       </div>
     </>
